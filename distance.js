e589976122424dd9ef4e65234060bdbb90d94274
qingbo93 <qingbo1993@gmail.com>
2017-02-19 22:20:28 -0500

    Save point, redesigned most layouts, need to add google map,experience + work projects

--- distance.js
+++ distance.js
@@ -1,26 +1,26 @@
 function CalcDist(data) {
-	var x1=data.x1;
-	var y1=data.y1;
-	var x2=data.x2;
-	var y2=data.y2;
-  
-	var lat1 = x1* Math.PI/180;;
-	var lon1 = y1* Math.PI/180;;
-	var lat2 = x2 * Math.PI/180;;
-	var lon2 = y2* Math.PI/180;;
-	
-	var lat=lat2-lat1;
-	var lon=lon2-lon1;
-  
-	var a  = Math.pow(Math.sin(lat/2),2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(lon/2),2);
-	var	c  = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
-	dkm = c * 6373; 
-    dkm = Math.round( dkm * 1000) / 1000;
-  
-  close();
+    var x1 = data.x1;
+    var y1 = data.y1;
+    var x2 = data.x2;
+    var y2 = data.y2;
+
+    var lat1 = x1 * Math.PI / 180;;
+    var lon1 = y1 * Math.PI / 180;;
+    var lat2 = x2 * Math.PI / 180;;
+    var lon2 = y2 * Math.PI / 180;;
+
+    var lat = lat2 - lat1;
+    var lon = lon2 - lon1;
+
+    var a = Math.pow(Math.sin(lat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(lon / 2), 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    dkm = c * 6373;
+    dkm = Math.round(dkm * 1000) / 1000;
+
+    close();
 }
-self.onmessage=function(evt){
-	
-	CalcDist(evt.data);
-	postMessage("The Distance is: "+dkm+"km.");
+self.onmessage = function(evt) {
+
+    CalcDist(evt.data);
+    postMessage("The Distance is: " + dkm + "km.");
 }
--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-$(document).ready(function(){
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-      window.location.hash = hash;
-      });
-    }
-  });
-})
-	$(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 500) {
-          $(this).addClass("slide");
+$(document).ready(function() {
+    $(".navbar a, a[href='#about']").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
         }
     });
-  });
+})
+$(window).scroll(function() {
+    $(".slideanim").each(function() {
+        var pos = $(this).offset().top;
 
-	function onVideoClick(theLink) {
-	    document.getElementById("video_pop").innerHTML = "<video autoplay muted loop id=\"the_Video\"><source src=\""+theLink+"\" type=\"video/webm\"></video>";
-	    document.getElementById("video_pop").style.display="block";
-	}
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 500) {
+            $(this).addClass("slide");
+        }
+    });
+});
 
-	function onPopClick() {
-	    document.getElementById("video_pop").style.display="none";
-	    document.getElementById("video_pop").innerHTML = "";
-	}
+function onVideoClick(theLink) {
+    document.getElementById("video_pop").innerHTML = "<video autoplay muted loop id=\"the_Video\"><source src=\"" + theLink + "\" type=\"video/webm\"></video>";
+    document.getElementById("video_pop").style.display = "block";
+}
+
+function onPopClick() {
+    document.getElementById("video_pop").style.display = "none";
+    document.getElementById("video_pop").innerHTML = "";
+}
+
+function myFunction() {
+    var navbar = document.getElementById("aNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className += " w3-animate-top";
+        navbar.className = navbar.className.replace("w3-opacity", "");
+    } else {
+        navbar.className += " w3-opacity";
+        navbar.className = navbar.className.replace("w3-animate-top", "");
+    }
+}
+$(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 500) {
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
