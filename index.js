e5a2d38b91bcb8beb8b1545369cdf1233d8156ba
qingbo93 <qingbo1993@gmail.com>
2017-02-20 00:24:27 -0500

    added map

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function myMap() {
+var myCenter = new google.maps.LatLng(43.894945, -79.420743);
+var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
+}
 $(document).ready(function() {
     $(".navbar a, a[href='#about']").on('click', function(event) {
         if (this.hash !== "") {
